f4763f94a69e2d970dea9f878c4f03cdd128879e
RaviyaSL <ravin.19@cse.mrt.ac.lk>
2021-09-23 11:37:44 +0530

    "MATH OPERATION-sumOfPrimes"

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -15,4 +15,47 @@ public class MathOperation {
         return operand1 - operand2;
     }
 
+    public static int sumOfPrimes(int n){
+            int sum = 0;
+            for (int p : listPrimes(n- 1))
+                sum += p;
+            return sum;
+    }
+    public static int[] listPrimes(int n) {
+        boolean[] isPrime = listPrimality(n);
+        int count = 0;
+        for (boolean b : isPrime) {
+            if (b)
+                count++;
+        }
+
+        int[] result = new int[count];
+        for (int i = 0, j = 0; i < isPrime.length; i++) {
+            if (isPrime[i]) {
+                result[j] = i;
+                j++;
+            }
+        }
+        return result;
+    }
+
+    public static boolean[] listPrimality(int n) {
+        if (n < 0)
+            throw new IllegalArgumentException("Negative array size");
+        boolean[] result = new boolean[n + 1];
+        if (n >= 2)
+            result[2] = true;
+        for (int i = 3; i <= n; i += 2)
+            result[i] = true;
+        // Sieve of Eratosthenes
+        for (int i = 3, end = (int)Math.sqrt(n); i <= end; i += 2) {
+            if (result[i]) {
+                // Note: i * i does not overflow
+                for (int j = i * i, inc = i * 2; j <= n; j += inc)
+                    result[j] = false;
+            }
+        }
+        return result;
+    }
+
 }
