5d65bb99a38765c14fa0c8d4f1260fe642b10d8a
samadhi-k <pereradivya3@gmail.com>
2021-09-23 11:37:10 +0530

    "MATHOPERATION: sumOfIntegers"

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -5,14 +5,19 @@ import java.util.Arrays;
 
 public class MathOperation {
 
-    public static double add(double... operands)
-    {
+    public static double add(double... operands) {
         return Arrays.stream(operands).reduce(0d, Double::sum);
     }
 
-    public static double subtract(double operand1, double operand2)
-    {
+    public static double subtract(double operand1, double operand2) {
         return operand1 - operand2;
     }
 
+    public static int sumOfIntegers(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 }
