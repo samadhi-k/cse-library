17e50cad8c4da64f8db79e33b9a6c88d286f1030
Nimsara Fernando <nimsara66@gmail.com>
2021-09-23 11:25:30 +0530

    implement fib function

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -2,17 +2,21 @@ package it.uom.cse;
 
 import java.util.Arrays;
 
-
 public class MathOperation {
 
-    public static double add(double... operands)
-    {
+    public static double add(double... operands) {
         return Arrays.stream(operands).reduce(0d, Double::sum);
     }
 
-    public static double subtract(double operand1, double operand2)
-    {
+    public static double subtract(double operand1, double operand2) {
         return operand1 - operand2;
+    }
+
+    public static int fib(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
     }
 
 }
